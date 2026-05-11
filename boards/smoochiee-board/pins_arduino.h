8ff766a84c74cc05a13541271877e45c8fe0e0da
a62002137-cyber <a62002137@gmail.com>
2026-05-11 18:49:30 +0700

    Refactor pins_arduino.h by removing unused definitions
    Removed commented-out definitions for CC1101 and NRF24 modules, and cleaned up the file.

--- boards/smoochiee-board/pins_arduino.h
+++ boards/smoochiee-board/pins_arduino.h
@@ -38,20 +38,20 @@ static const uint8_t SCK = 18;
 #define LED_ON HIGH
 #define LED_OFF LOW
 
-#define USE_CC1101_VIA_SPI
-#define CC1101_GDO0_PIN 9
-#define CC1101_GDO2_PIN 10
-#define CC1101_SS_PIN 46
-#define CC1101_MOSI_PIN SPI_MOSI_PIN
-#define CC1101_SCK_PIN SPI_SCK_PIN
-#define CC1101_MISO_PIN SPI_MISO_PIN
+//#define USE_CC1101_VIA_SPI
+//#define CC1101_GDO0_PIN 9
+//#define CC1101_GDO2_PIN 10
+//#define CC1101_SS_PIN 46
+//#define CC1101_MOSI_PIN SPI_MOSI_PIN
+//#define CC1101_SCK_PIN SPI_SCK_PIN
+//#define CC1101_MISO_PIN SPI_MISO_PIN
 
-#define USE_NRF24_VIA_SPI
-#define NRF24_CE_PIN 21
-#define NRF24_SS_PIN 14
-#define NRF24_MOSI_PIN SPI_MOSI_PIN
-#define NRF24_SCK_PIN SPI_SCK_PIN
-#define NRF24_MISO_PIN SPI_MISO_PIN
+//#define USE_NRF24_VIA_SPI
+//#define NRF24_CE_PIN 21
+//#define NRF24_SS_PIN 14
+//#define NRF24_MOSI_PIN SPI_MOSI_PIN
+//#define NRF24_SCK_PIN SPI_SCK_PIN
+//#define NRF24_MISO_PIN SPI_MISO_PIN
 
 #define FP 1
 #define FM 2
@@ -80,10 +80,10 @@ static const uint8_t SCK = 18;
 #define SPI_READ_FREQUENCY 20000000
 #define SPI_TOUCH_FREQUENCY 2500000
 
-#define SDCARD_CS 3
-#define SDCARD_SCK 18
-#define SDCARD_MISO 8
-#define SDCARD_MOSI 17
+//#define SDCARD_CS 3
+//#define SDCARD_SCK 18
+//#define SDCARD_MISO 8
+//#define SDCARD_MOSI 17
 
 #define GROVE_SDA 47
 #define GROVE_SCL 48
